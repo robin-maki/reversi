5cc81fa4e5a44f41141f71e56674e98a0fd17024
serendipity <fe91990@gmail.com>
2019-05-01 21:16:48 +0900

    Update Game.py
    1. add code that change state of clicked block

--- Game.py
+++ Game.py
@@ -1,49 +1,42 @@
+import blockclass as bc
 DIRECTION = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 
-class Game:
-    def __init__(self):
-        self.gameBoard = []
-        for i in range(8):
-            self.gameBoard.append([0, 0, 0, 0, 0, 0, 0, 0])
-        self.gameBoard[3][3] = self.gameBoard[4][4] = 1
-        self.gameBoard[3][4] = self.gameBoard[4][3] = -1
 
-    def checkFlippable(self, x, y, my):
-        if self.gameBoard[x][y] != 0:
-            return []
-        flippable = []
-        for d in DIRECTION:
-            tx = x
-            ty = y
-            possible = []
-            while 1:
-                tx += d[0]
-                ty += d[1]
-                if (not (0 <= tx <= 7 and 0 <= ty <= 7)) or self.gameBoard[tx][ty] == 0:
-                    break
-                if self.gameBoard[tx][ty] == my:
-                    flippable.extend(possible)
-                    break
-                else:
-                    possible.append((tx, ty))
-        return flippable
+       
+def checkFlippable(x, y, my):
+    if bc.board[x][y].blockstate != 0:
+        return []
+    flippable = []
+    for d in DIRECTION:
+        tx = x
+        ty = y
+        possible = []
+        while 1:
+            tx += d[0]
+            ty += d[1]
+            if (not (0 <= tx <= 7 and 0 <= ty <= 7)) or bc.board[tx][ty].blockstate == 0:
+                break
+            if bc.board[tx][ty].blockstate == my:
+                flippable.extend(possible)
+                break
+            else:
+                possible.append((tx, ty))
+    return flippable
 
-    def getPlaceable(self, my):
-        flippable = []
-        for i in range(8):
-            for j in range(8):
-                if self.gameBoard[i][j] == 0 and len(self.checkFlippable(i, j, my)) > 0:
-                    flippable.append((i, j))
-        return flippable
+def getPlaceable(my):
+    flippable = []
+    for i in range(8):
+        for j in range(8):
+            if bc.board[i][j].blockstate == 0 and len(checkFlippable(i, j, my)) > 0:
+                flippable.append((i, j))
+    return flippable
 
-    def place(self, x, y, my):
-        toFlip = self.checkFlippable(x, y, my)
-        if len(toFlip) > 0:
-            for f in toFlip:
-                self.gameBoard[f[0]][f[1]] = my
-            return toFlip
-        else:
-            raise ValueError('Invalid place')
-
-
-g = Game()
+def place(x, y, my):
+    toFlip = checkFlippable(x, y, my)
+    if len(toFlip) > 0:
+        bc.board[x][y].blockstate = my
+        for f in toFlip:
+            bc.board[f[0]][f[1]].blockstate = my
+        return toFlip
+    else:
+        raise ValueError('Invalid place')
