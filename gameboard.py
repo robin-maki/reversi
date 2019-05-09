528b8fde8fc16c59c2d39b535076c5d8879128e5
serendipity <fe91990@gmail.com>
2019-05-09 13:31:09 +0900

    시발
    다고치고불러

--- gameboard.py
+++ gameboard.py
@@ -1,7 +1,7 @@
 import pygame
 import blockclass
-import search,game
-blockimage = pygame.image.load('pad.png')
+import Game
+blockimage = pygame.image.load('pad1.png')
 
 window_width = 512
 window_height = 512
@@ -18,6 +18,8 @@ def rungame():
     gamepad.blit(background,(0,0))
     crashed = False
     i = 0
+    howmuch_block = 0
+    gamecontinue = True
     
     gamepad.blit(icon1,(192,192))
     gamepad.blit(icon1,(256,256))
@@ -35,13 +37,13 @@ def rungame():
                 cliy = pos[1]//64
                     
                     
-                if i%2 == 0 :#사용자 턴
+                if i%2 == 0 and gamecontinue :#사용자 턴
                     
-                    if blockclass.board[clix][cliy].blockstate == 0 and len(game.checkFlippable(clix, cliy, -1)) > 0:
-                        print('뒤집을수잇는거:',game.checkFlippable(clix, cliy, -1))
-                        toFlip = game.place(clix, cliy, -1)
-                        if len(toFlip) > 0:
-                            for f in toFlip:
+                    if blockclass.board[clix][cliy].blockstate == 0 and len(Game.checkFlippable(clix, cliy, -1)) > 0:
+                        toFlip_player = Game.place(clix, cliy, -1)
+                        if len(toFlip_player) > 0:
+                            howmuch_block += 1
+                            for f in toFlip_player:
                                 gamepad.blit(icon1,(blockclass.board[f[0]][f[1]].block_x,blockclass.board[f[0]][f[1]].block_y))
                             gamepad.blit(icon1,(blockclass.board[clix][cliy].block_x,blockclass.board[clix][cliy].block_y))
                         
@@ -49,22 +51,24 @@ def rungame():
                         i = i + 1
                     
                     
-                elif i%2 == 1 : # 컴퓨터 턴
+                elif i%2 == 1 and gamecontinue : # 컴퓨터 턴
                     
-                    if blockclass.board[clix][cliy].blockstate == 0 and len(game.checkFlippable(clix,cliy, 1)) > 0:    
-                        print('뒤집을수잇는거:',game.checkFlippable(clix, cliy, 1))
-                        toFlip = game.place(clix, cliy, 1)
-                        if len(toFlip) > 0:
-                            for f in toFlip:
+                    if blockclass.board[clix][cliy].blockstate == 0 and len(Game.checkFlippable(clix,cliy, 1)) > 0:    
+                        toFlip_com = Game.place(clix, cliy, 1)
+                        if len(toFlip_com) > 0:
+                            howmuch_block += 1
+                            for f in toFlip_com:
                                 gamepad.blit(icon2,(blockclass.board[f[0]][f[1]].block_x,blockclass.board[f[0]][f[1]].block_y))
                             gamepad.blit(icon2,(blockclass.board[clix][cliy].block_x,blockclass.board[clix][cliy].block_y))
                         
                         
                         i = i + 1
+                        
+                        #하우머치블록 64개이거나 곱하기0ㅇ일시 게임종료
+                if (len(toFlip_player) == 0 and len(toFilp_com) == 0) or howmuch_block == 64 :
+                    gamecontinew = False
+                    gamepad.blit(text, text.get_rect())
                     
-                print('말종류:',blockclass.board[clix][cliy].blockstate)
-        
-                print('i:',i,'x랑y',clix,cliy)
         pygame.display.update()
         clock.tick(30)
     pygame.quit()
@@ -80,11 +84,17 @@ def initgame():
     gamepad = pygame.display.set_mode((window_width,window_height))
     background = pygame.image.load('pad1.png')
     icon1 = pygame.image.load('icon1.png')
-    icon2 = pygame.image.load('icon2.png')
+    icon2 = pygame.image.load('icon2_2.png')
     
     pygame.display.set_caption('reversi')
     
     clock = pygame.time.Clock()
+    
+    font = pygame.font.Font("freesansbold.ttf", 40)
+    text = font.render(u"안녕하세요", True, (255, 0, 0))
+
+    
+
     
     
     
