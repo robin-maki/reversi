70987e71e9cea8f62b10c6185676aadde8952262
robin* <robin_maki@planet.moe>
2019-05-11 06:36:59 +0900

    입력 텐서를 [64]에서 [8, 8]로 변경

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
-x = tf.placeholder(tf.float32, [1, 64])
+x = tf.placeholder(tf.float32, [None, 8, 8])
+y = tf.reshape(x, [-1, 64])
 
 w = tf.Variable(tf.random_uniform([64, 1], 0.0, 1.0))
 
-h = tf.matmul(x, w)
+h = tf.matmul(y, w)
 
 
 def run(gameBoard):
@@ -13,8 +14,8 @@ def run(gameBoard):
         tensor.extend(i)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    res = sess.run(h, feed_dict={x: [tensor]})
-    return res[0][0]
+    res = sess.run(h, feed_dict={x: [gameBoard]})
+    return res
 
 
 print(run([[0, 0, 0, 0, 0, 0, 0, 0],
