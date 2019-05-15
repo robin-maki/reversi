c724603f60573dc545b23f5dfd40802250d5a190
serendipity <fe91990@gmail.com>
2019-05-15 17:15:30 +0900

    게임 결과 출력 버그 수정
    햇음

--- gameboard.py
+++ gameboard.py
@@ -4,10 +4,19 @@ import Game
 blockimage = pygame.image.load('pad1.png')
 
 window_width = 512
-window_height = 512
+window_height = 576
 colour = (255,255,255)
+BLACK = (0,0,0)
 
 game = Game.Game()
+
+def make_text(text,x, y):
+    font = pygame.font.Font('freesansbold.ttf', 25)
+    surf = font.render(text, True, BLACK, colour)
+    rect = surf.get_rect()
+    rect.center = (x, y)
+    gamepad.blit(surf, rect)
+    return rect
 
 
 
@@ -19,9 +28,8 @@ def rungame():
     crashed = False
     i = 0
     gamecontinue = True
-    howmuch_block = 4
-    available_player,available_com = 1,1
-
+    point_com = 2
+    point_player = 2
     gamepad.blit(icon1,(192,192))
     gamepad.blit(icon1,(256,256))
     gamepad.blit(icon2,(192,256))
@@ -37,33 +45,39 @@ def rungame():
                 clix = pos[0]//64
                 cliy = pos[1]//64
                 if i%2 == 0 and gamecontinue :#사용자 턴
-                    available_player = len(game.checkFlippable(clix, cliy, -1))
+            
                     if game.gameBoard[clix][cliy] == 0 and len(game.checkFlippable(clix, cliy, -1)) > 0:
+    
                         toFlip_player = game.place(clix, cliy, -1)
+                        
                         if len(toFlip_player) > 0:
-                            howmuch_block += 1
                             for f in toFlip_player:
                                 gamepad.blit(icon1,(blockclass.board[f[0]][f[1]].block_x,blockclass.board[f[0]][f[1]].block_y))
                             gamepad.blit(icon1,(blockclass.board[clix][cliy].block_x,blockclass.board[clix][cliy].block_y))
-
+                            point_player = point_player + len(toFlip_player)+1
+                            point_com = point_com - len(toFlip_player)
                     i = i + 1
 
                 elif i%2 == 1 and gamecontinue : # 컴퓨터 턴
-                    available_com = len(game.checkFlippable(clix, cliy, 1))
+
                     if game.gameBoard[clix][cliy] == 0 and len(game.checkFlippable(clix,cliy, 1)) > 0:
+                        
                         toFlip_com = game.place(clix, cliy, 1)
                         if len(toFlip_com) > 0:
-                            howmuch_block += 1
                             for f in toFlip_com:
                                 gamepad.blit(icon2,(blockclass.board[f[0]][f[1]].block_x,blockclass.board[f[0]][f[1]].block_y))
                             gamepad.blit(icon2,(blockclass.board[clix][cliy].block_x,blockclass.board[clix][cliy].block_y))
+                            point_player = point_player - len(toFlip_player)
+                            point_com = point_com + len(toFlip_player)+1                    
+                    
                     i = i + 1
 
-                        #하우머치블록 64개이거나
+                       
                        
                 if len(game.getPlaceable(1))==0 and len(game.getPlaceable(-1))==0 :
                     gamepad.blit(gameover, (0,32))
                     gamecontinue = False
+                    make_text('you '+str(point_player)+' vs '+' computer '+str(point_com),256, 128)
                 print(len(game.getPlaceable(1)),len(game.getPlaceable(-1)))               
 
         pygame.display.update()
