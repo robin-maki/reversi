ec2bb8625f9b43af1ad0d2351039d6da9d6d2725
robin* <robin_maki@planet.moe>
2019-05-20 19:51:36 +0900

    function docs 추가

--- game.py
+++ game.py
@@ -5,6 +5,10 @@ DIRECTION = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1
 
 class Game:
     def __init__(self, gameBoard=None):
+        """
+        Game 클래스의 생성자
+        :param gameBoard?: (List[8][8]) 게임의 초기 상태 List, 주어지지 않으면 게임 초기상태로 초기화한다
+        """
         if gameBoard is None:
             self.gameBoard = []
             for i in range(8):
@@ -15,6 +19,13 @@ class Game:
             self.gameBoard = copy.deepcopy(gameBoard)
 
     def checkFlippable(self, x, y, my):
+        """
+        주어진 위치에 돌을 놓을때 뒤집을 수 있는 돌의 위치 List를 리턴한다.
+        :param x: (int) 돌을 놓을 위치의 x좌표
+        :param y: (int) 돌을 놓을 위치의 y좌표
+        :param my: (int) 자신의 색깔(-1/1)
+        :return: (List[][2]) 뒤집을 수 있는 돌의 위치들
+        """
         if self.gameBoard[x][y] != 0:
             return []
         flippable = []
@@ -35,6 +46,11 @@ class Game:
         return flippable
 
     def getPlaceable(self, my):
+        """
+        게임에서 돌을 놓을 수 있는(=하나라도 뒤집을 수 있는) 위치 List를 리턴한다.
+        :param my: (int) 자신의 색깔(-1/1)
+        :return: (List[][2]) 놓을 수 있는 돌의 위치들
+        """
         flippable = []
         for i in range(8):
             for j in range(8):
@@ -43,6 +59,13 @@ class Game:
         return flippable
 
     def place(self, x, y, my):
+        """
+        돌을 놓고 뒤집는다. 만약 뒤집을 수 없는 위치라면 ValueError를 일으킨다. 실제로 판을 변경한다.
+        :param x: (int) 돌을 놓을 위치의 x좌표
+        :param y: (int) 돌을 놓을 위치의 y좌표
+        :param my: (int) 자신의 색깔(-1/1)
+        :return: (List[][2]) 놓거나 뒤집은 돌들의 좌표들
+        """
         toFlip = self.checkFlippable(x, y, my)
         if len(toFlip) > 0:
             toFlip.append((x, y))
@@ -53,11 +76,22 @@ class Game:
             raise ValueError('Invalid place')
 
     def getPotentialBoard(self, x, y, my):
+        """
+        해당 위치에 돌을 놓았을 때 결과적으로 만들어지는 gameBoard를 리턴한다. 실제로 판을 변경하지는 않는다.
+        :param x: (int) 돌을 놓을 위치의 x좌표
+        :param y: (int) 돌을 놓을 위치의 y좌표
+        :param my: (int) 자신의 색깔(-1/1)
+        :return: (List[8][8]) 해당 위치에 돌을 놓은 후의 gameBoard
+        """
         potentialGame = Game(self.gameBoard)
         potentialGame.place(x, y, my)
         return potentialGame.gameBoard
 
     def getScore(self):
+        """
+        각각 돌의 갯수를 세서 리턴한다.
+        :return: (List[2]) 1, -1의 돌 갯수 List
+        """
         score = [0, 0]
         for i in range(8):
             for j in range(8):
