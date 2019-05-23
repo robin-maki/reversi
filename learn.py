f05ff8c3e0f709b5b851bc3ecc33fb82d1fc2db1
robin* <robin_maki@planet.moe>
2019-05-23 11:38:46 +0900

    학습 결과를 저장/불러오는 기능 추가

--- learn.py
+++ learn.py
@@ -1,8 +1,9 @@
 import neuralnet
 import game
+import json
 
 LEARNING_COUNT = 12
-WIN_COUNT = 8
+WIN_COUNT = 4
 
 assert WIN_COUNT * (WIN_COUNT - 1) / 2 <= LEARNING_COUNT, '생존 신경망이 너무 많다!!'
 
@@ -10,10 +11,37 @@ learningList = []
 leagueNum = 0
 
 def init():
-    for i in range(LEARNING_COUNT):
-        learningList.append(neuralnet.NeuralNet())
+    from pathlib import Path
+    if Path('learn.json').is_file():
+        print('File exists')
+        load()
+    else:
+        print('File not exists. Initializing')
+        for i in range(LEARNING_COUNT):
+            learningList.append(neuralnet.NeuralNet())
+        save()
+
+def save():
+    with open('learn.json', 'w') as jsonFile:
+        weights = []
+        for l in learningList:
+            weights.append(l.weight)
+        jsonFile.write(json.dumps(weights))
+        jsonFile.close()
+
+def load():
+    with open('learn.json', 'r') as jsonFile:
+        global learningList
+        loadList = []
+        for w in json.load(jsonFile):
+            loadList.append(neuralnet.NeuralNet(w))
+        learningList = loadList
+        jsonFile.close()
 
 def league():
+    global learningList, leagueNum
+    leagueNum += 1
+    winlose = 0
     print('[League {}] Start!'.format(leagueNum))
     for i in range(LEARNING_COUNT):
         learningList[i].winScore = 0
@@ -33,10 +61,11 @@ def league():
                 if predict1 is None and predict2 is None:
                     score = leagueGame.getScore()
                     if score[0] > score[1]:
-                        learningList[i].winScore += 1
+                        learningList[i].winScore += (score[0] - score[1])
                     elif score[0] < score[1]:
-                        learningList[j].winScore += 1
+                        learningList[j].winScore += (score[1] - score[0])
                     print('[League {}] Game over! {}:{}'.format(leagueNum, score[0], score[1]))
+                    '''
                     for u in range(8):
                         for v in range(8):
                             if leagueGame.gameBoard[u][v] == 1:
@@ -46,7 +75,13 @@ def league():
                             else:
                                 print('0', end='')
                         print('')
+                    '''
+                    if score[0] > score[1]:
+                        winlose += 1
+                    elif score[0] < score[1]:
+                        winlose -= 1
                     break
+    print('[League {}] W/L Point: {}'.format(leagueNum, winlose))
     learningList.sort(key=lambda nn: nn.winScore, reverse=True)
     newLearningList = []
     for i in range(WIN_COUNT):
@@ -54,8 +89,5 @@ def league():
             newLearningList.append(learningList[i].gen(learningList[j]))
     while len(newLearningList) < LEARNING_COUNT:
         newLearningList.append(neuralnet.NeuralNet())
-    globals().learningList = newLearningList
-
-
-init()
-league()
+    learningList = newLearningList
+    save()
